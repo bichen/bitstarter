7bf211e1e7888ccc4423210df4f2de5d4d6eb497
Bing Chen <gubami@hotmail.com>
2013-07-11 01:29:23 +0000

    Hopefully completed Part 1 of Homework 3

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2');
+    var data;
+    var fileStream = require('fs');
+    data = fileStream.readFileSync('index.html');
+    response.send(data.toString('utf-8', 0, data.length));
 });
 
 var port = process.env.PORT || 5000;
